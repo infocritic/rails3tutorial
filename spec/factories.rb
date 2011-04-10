7ccc17adb48cfd004e63ccab63709964e0d0dda3
John Meyer <infocritic@gmail.com>
2011-04-10 06:21:14 -0700

    Added a User factory and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "John Meyer"
+  user.email                 "infocritic@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
